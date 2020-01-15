a2c217d34292162538841c0f6e48324963c28b2d
S4RPYK3IVl5 <andreysaprykin@mail.ru>
2020-01-15 08:58:23 +0400

    init spring project

---
+++ Backend/daa/src/test/java/ru/quillaer/daa/DaaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.quillaer.daa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
