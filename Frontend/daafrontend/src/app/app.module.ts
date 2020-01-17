8f11beca532e2c6c0957bc9e6a5531b072f72a6a
Ilya <Ilya.pospelov2000@gmail.com>
2020-01-17 18:23:16 +0100

    simple variation of the site

--- Frontend/daafrontend/src/app/app.module.ts
+++ Frontend/daafrontend/src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MaterialModule } from './material/material.module';
+import { LayoutComponent } from './layout/layout.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { HomeComponent } from './home/home.component';
+import {RoutingModule} from './routing/routing.module';
+import { HeaderComponent } from './navigation/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent,
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NoopAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatSidenavModule,
+    BrowserModule,
+    MaterialModule,
+    FlexLayoutModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/daafrontend/src/app/material/material.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule, MatSidenavModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  exports: [
+    MatSidenavModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+
+  ],
+})
+export class MaterialModule { }
